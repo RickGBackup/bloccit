5a2611de5e8982f128024e8ad9ac103f2abb2ee8
Rick G <richkws@gmail.com>
2018-01-17 22:40:19 +0000

    Checkpoint 36 Rails-CRUD: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   
   def censor_spam
+    #censor the title of the first post, and every fifth post thereafter
     if self.id == 1 || (self.id - 1) % 5 == 0
       self.title = "SPAM"
     end
