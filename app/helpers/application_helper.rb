1f1dc3bc984c9c373d4a3ea657b805354cfcc538
Rick G <richkws@gmail.com>
2018-01-27 15:57:48 +0000

    Commented form_group_tag in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,3 +6,35 @@ module ApplicationHelper
     content_tag :div, capture(&block), class: css_class
   end
 end
+
+# Explaining form_group_tag to myself:
+
+# form_group_tag takes as arguments the errors array, and a Proc, which works like a reusuable code block.
+# we will use form_group_tag in forms.
+# It returns different HTML content depending on whether the errors array is empty or not.
+
+# capture(&block) captures the return value of &block.
+
+# content_tag return a block of HTML code.
+
+# How form_group_tag is used:
+
+#In posts/views/_forms.html.erb, we call:
+
+# <%= form_group_tag(post.errors[:title]) do %>
+  #1
+#   <%= f.label :title %>
+#   <%= f.text_field :title, class: 'form-control', placeholder: "Enter post title" %>
+
+# <% end %>
+
+# It returns a div containing the return value of the inner block #1,
+# with the css-class set to 'form-group' on 'form-group has-error', depending 
+# # whether there was an error.
+
+# So, if there was an error, form_group_tag will generate HTML that display the error, 
+# otherwise it generates standard form-group html.
+
+
+
+
