943dfb40a8531410cf0abaa8dc1d9fe50e0c411e
Rick G <richkws@gmail.com>
2018-02-03 11:49:13 +0000

    Assignment 47 Posts and Users: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,12 @@ class Post < ActiveRecord::Base
   
   
   default_scope  { order('created_at DESC') }  #Instruct rails to retrieve posts from the database by their created_at value, in descending order.
+  scope :ordered_by_title, -> { order('title DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+  
   #Scopes allow you to name and chain together SQL commands to select specific objects in a specific order.
+  #Defining scopes in the model gives us new method calls for making DB queries.
+  
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
