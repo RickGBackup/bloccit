c75688484fd3cef86e591ffeaf057deb0c8d9743
Rick G <richkws@gmail.com>
2018-02-21 15:47:17 +0000

    Checkpoint 54  - 50% Done

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,6 +7,10 @@ class Topic < ActiveRecord::Base
   
   has_many :comments, as: :commentable
   
+  scope :visible_to, -> (user) { user ? all : where(public: true) }  # scope generates the method visible_to(user).
+  #visible_to(user) runs the logic in the block on the relation/class it is called on.
+  # It returns either Topic.all or Topic.where(pubic: true), dependent on truthiness of its argument, user.
+  
   validates :name, presence: true, length: {minimum: 5}
   validates :description, presence: true, length: {minimum: 15}
 end
